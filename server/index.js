6496582d54e7304cdd996d65d9db202a502449f3
sonivipin119 <sonivipin232@gmail.com>
2025-09-07 14:36:07 +0530

    intial making structure

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Example route
+app.get("/", (req, res) => {
+  res.send("RxGuard backend is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
