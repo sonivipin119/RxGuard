9b6ec69b1049c820417957adb0fabbde2f7dfd67
sonivipin119 <sonivipin232@gmail.com>
2025-09-07 15:06:52 +0530

    structuring

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
